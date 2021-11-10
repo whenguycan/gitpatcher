dc822d012d9c92a048184e1333f1a9ae6bc2130b
wangcy <wangcy@goisan.cn>
2021-11-10 17:13:20 +0800

    工具类增加获取当前时间戳 git忽略文件更新

--- src/main/java/com/kaltsit/gitpatcher/util/StringUtils.java
+++ src/main/java/com/kaltsit/gitpatcher/util/StringUtils.java
@@ -1,5 +1,8 @@
 package com.kaltsit.gitpatcher.util;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 /**
  * @author wangcy
  * @date 2021/11/9 13:47
@@ -12,6 +15,10 @@ public class StringUtils {
 
     public static boolean isNotEmpty(String str) {
         return !isEmpty(str);
+    }
+
+    public static String getCurrentTimestamp() {
+        return DateTimeFormatter.ofPattern("_yyyy_MM_dd_HH_mm_ss").format(LocalDateTime.now());
     }
 
 }
